4c218e2c2ffae2706881acef88840c470d995bc9
Ramy Apepone <B00091062@mytudublin.ie>
2021-04-14 17:27:33 +0100

    initial commit

--- resources/views/customers/new.blade.php
+++ resources/views/customers/new.blade.php
@@ -1,3 +1,4 @@
+
 <FORM method="POST" action="/customers/create"> @csrf 
      Enter your first name:<input type="text" name="firstname"><br> 
      Enter your surname:<input type="text" name="surname"><br>
