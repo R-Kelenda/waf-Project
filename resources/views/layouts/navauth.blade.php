424c6ea722b2ee0cb22c61ed5405c1d4801b6f4b
Ramy Apepone <B00091062@mytudublin.ie>
2021-04-27 19:17:24 +0100

    added login, logout icon

--- resources/views/layouts/navauth.blade.php
+++ resources/views/layouts/navauth.blade.php
@@ -1,5 +1,8 @@
 <ul class="nav navbar-nav pull-right"> 
-    <li><a href="{{route('register')}}">Register</a></li> 
-    <li><a href="{{route('login')}}">Login</a></li> 
-    <li><a href="{{route('logout')}}">Logout</a></li> 
+    @if(Auth::guest())
+        <li><a href="{{route('register')}}"><span class ="glyphicon glyphicon-pencil"></a></li> 
+        <li><a href="{{route('login')}}"><span class ="glyphicon glyphicon-log-in"></a></li> 
+    @else
+	    <li><a href="{{route('logout')}}"><span class ="glyphicon glyphicon-log-out"></a></li> 
+    @endif
 </ul>
